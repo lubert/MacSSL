20d24b83f6c03507ee70c6b85a656574bdf2e7f8
Lu Zhu <misterzhu@gmail.com>
2025-09-17 20:39:43 -0700

    Add more conf

--- config_minimal.h
+++ config_minimal.h
@@ -10,12 +10,18 @@
 
 /* Core system support */
 #define MBEDTLS_HAVE_INT64              /* GCC supports 64-bit integers */
+/* DON'T define MBEDTLS_HAVE_ASM - conflicts with MBEDTLS_HAVE_INT64 */
+
+/* Explicitly disable time functions and threading for Classic Mac OS */
+#undef MBEDTLS_HAVE_TIME      /* Explicitly disable time functions */
+#undef MBEDTLS_THREADING_C    /* Classic Mac OS doesn't have POSIX threads */
+#undef MBEDTLS_THREADING_PTHREAD
 
 /* Memory and platform */
 #define MBEDTLS_PLATFORM_MEMORY
 #define MBEDTLS_NO_PLATFORM_ENTROPY
 #define MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
-#define MBEDTLS_NO_UDBL_DIVISION
+#define MBEDTLS_NO_UDBL_DIVISION        /* Use portable C implementation */
 
 /* Essential crypto modules */
 #define MBEDTLS_AES_C
@@ -64,14 +70,22 @@
 #define MBEDTLS_MEMORY_ALIGN_MULTIPLE 4
 #define MBEDTLS_MEMORY_BUFFER_ALLOC_C
 
+/* Platform abstraction layer configured in retro68_compat.c */
+
+/* GCC-specific optimizations for size */
+#ifdef __GNUC__
+#define MBEDTLS_OPTIMIZE_TININESS
+#endif
+
 /* Key exchange */
 #define MBEDTLS_KEY_EXCHANGE_RSA_ENABLED
 #define MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED
 #define MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
 
-/* Elliptic curves */
+/* Elliptic curves - disable restartable ECP to save memory */
 #define MBEDTLS_ECP_DP_SECP256R1_ENABLED
 #define MBEDTLS_ECP_DP_SECP384R1_ENABLED
+/* Note: MBEDTLS_SSL_ECP_RESTARTABLE disabled for Classic Mac OS memory constraints */
 
 /* TLS versions - Only TLS 1.2 for Classic Mac OS compatibility */
 #define MBEDTLS_SSL_PROTO_TLS1_2
@@ -83,5 +97,19 @@
 /* Force TLS 1.2 maximum */
 #define MBEDTLS_SSL_MAX_MAJOR_VERSION MBEDTLS_SSL_MAJOR_VERSION_3
 #define MBEDTLS_SSL_MAX_MINOR_VERSION MBEDTLS_SSL_MINOR_VERSION_3
+
+/* Disable features we don't need for Classic Mac OS */
+#undef MBEDTLS_FS_IO
+#undef MBEDTLS_NET_C          /* We implement our own using Open Transport */
+#undef MBEDTLS_SSL_CACHE_C
+#undef MBEDTLS_SSL_SESSION_TICKETS
+#undef MBEDTLS_TIMING_C
+#undef MBEDTLS_HAVEGE_C
+#undef MBEDTLS_ZLIB_SUPPORT
+#undef MBEDTLS_PKCS11_C
+#undef MBEDTLS_PKCS12_C
+
+/* Custom entropy function for Classic Mac OS */
+#define MBEDTLS_ENTROPY_HARDWARE_ALT
 
 #endif /* MBEDTLS_CONFIG_H */
